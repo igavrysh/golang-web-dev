95d6b979612f77bf2b5f60413d616814598ce36d
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-03-27 09:54:28 -0700

    serving files with io.Copy

---
+++ 023_serving-files/03_ServeFile/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", rainbow)
+	http.HandleFunc("/rainbow.png", rainbowPic)
+	http.ListenAndServe(":8080", nil)
+}
+
+func rainbow(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `<img src="rainbow.png">`)
+}
+
+func rainbowPic(w http.ResponseWriter, req *http.Request) {
+	f, err := os.Open("rainbow.png")
+	if err != nil {
+		http.Error(w, "file not found", 404)
+		return
+	}
+	defer f.Close()
+
+	io.Copy(w, f)
+}
