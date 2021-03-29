2c4b68532d04480c8c90223fd3b069a38bb25bbe
Ievgen Gavrysh <zhenya.ga@gmail.com>
2021-03-28 23:39:09 -0700

    handson ex

---
+++ 024_hands-on/02_solution/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, "foo run")
+}
+
+func dog(w http.ResponseWriter, r *http.Request) {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+	err := tpl.ExecuteTemplate(w, "dog.gohtml", nil)
+	HandleError(w, err)
+}
+
+func dogPic(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "dog.jpg")
+}
+
+func HandleError(w http.ResponseWriter, err error) {
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatalln(err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/dog/", dog)
+	http.HandleFunc("/dog.jpg", dogPic)
+	http.ListenAndServe(":8080", nil)
+}
