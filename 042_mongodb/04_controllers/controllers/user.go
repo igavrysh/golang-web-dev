d66e4bec56e824116f0d062077e993bb74df4bb9
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-04-24 21:12:58 -0700

    controllers

---
+++ 042_mongodb/04_controllers/controllers/user.go
@@ -0,0 +1,62 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"example.com/m/models"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type UserController struct{}
+
+func NewUserController() *UserController {
+	return &UserController{}
+}
+
+func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, 
+	p httprouter.Params) {
+	u := models.User{
+		Name:   "James Bond",
+		Gender: "male",
+		Age:    32,
+		Id:     p.ByName("id"),
+	}
+
+	// Marshal into JSON
+	uj, _ := json.Marshal(u)
+
+	// Write content-type, statuscode, payload
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, 
+	p httprouter.Params) {
+	// composite literal - type and curly braces
+	u := models.User{}
+
+	// encode / decode for sending / receiving JSON to / from a stream
+	json.NewDecoder(r.Body).Decode(&u)
+
+	// Change Id
+	u.Id = "007"
+
+	// marshal / unmarshal for having JSON assigned to a variable
+	uj, _ := json.Marshal(u)
+
+	// Write content-type statuscode, payload
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated) // 201
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request,
+	 p httprouter.Params) {
+	// TODO: write code to delete user
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Write code to delete user\n")
+}
