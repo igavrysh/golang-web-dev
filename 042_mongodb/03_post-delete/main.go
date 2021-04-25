7c14d0cbc2d12ba3d2265b5e2846c5acf0167777
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-04-24 20:58:13 -0700

    post-delete

--- 042_mongodb/03_post-delete/main.go
+++ 042_mongodb/03_post-delete/main.go
@@ -14,6 +14,8 @@ func main() {
 	r.GET("/", index)
 	// added route plus parameter
 	r.GET("/user/:id", getUser)
+	r.POST("/user", createUser)
+	r.DELETE("/user/:id", deleteUser)
 	http.ListenAndServe("localhost:8080", r)
 }
 
@@ -50,4 +52,29 @@ func getUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func createUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	// composite literal - type and curly braces
+	u := models.User{}
+
+	// encode / decode for sending / receiving JSON to / from a stream
+	json.NewDecoder(r.Body).Decode(&u)
+
+	// Change Id
+	u.Id = "007"
+
+	// marshal / unmarshal for having JSON assigned to a variable
+	uj, _ := json.Marshal(u)
+
+	// Write content-type statuscode, payload
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated) // 201
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	// TODO: write code to delete user
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Write code to delete user\n")
 }
