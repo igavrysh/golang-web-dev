6c9748781a13f4ca05be2e8783b7b7e7d1b4cc1d
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-05-02 21:52:52 -0700

    refactoring assignment

---
+++ 042_mongodb/05_mongodb/11_solution/session/session.go
@@ -0,0 +1,79 @@
+package session
+
+import (
+	"example.com/m/models"
+	"fmt"
+	uuid "github.com/satori/go.uuid"
+	"net/http"
+	"time"
+)
+
+const Length int = 30
+
+var Users = map[string]models.User{}       // user ID, user
+var Sessions = map[string]models.Session{} // session ID, session
+var LastCleaned time.Time
+
+func GetUser(w http.ResponseWriter, req *http.Request) models.User {
+	// get cookie
+	c, err := req.Cookie("session")
+	if err != nil {
+		sID := uuid.NewV4()
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+
+	}
+	c.MaxAge = Length
+	http.SetCookie(w, c)
+
+	// if the user exists already, get user
+	var u models.User
+	if s, ok := Sessions[c.Value]; ok {
+		s.LastActivity = time.Now()
+		Sessions[c.Value] = s
+		u = Users[s.UserName]
+	}
+	return u
+}
+
+func AlreadyLoggedIn(w http.ResponseWriter, req *http.Request) bool {
+	c, err := req.Cookie("session")
+	if err != nil {
+		return false
+	}
+	s, ok := Sessions[c.Value]
+	if ok {
+		s.LastActivity = time.Now()
+		Sessions[c.Value] = s
+	}
+	_, ok = Users[s.UserName]
+	// refresh session
+	c.MaxAge = Length
+	http.SetCookie(w, c)
+	return ok
+}
+
+func Clean() {
+	fmt.Println("BEFORE CLEAN") // for demonstration purposes
+	Show()              // for demonstration purposes
+	for k, v := range Sessions {
+		if time.Now().Sub(v.LastActivity) > (time.Second * 30) {
+			delete(Sessions, k)
+		}
+	}
+	LastCleaned = time.Now()
+	fmt.Println("AFTER CLEAN") // for demonstration purposes
+	Show()             // for demonstration purposes
+}
+
+// for demonstration purposes
+func Show() {
+	fmt.Println("********")
+	for k, v := range Sessions {
+		fmt.Println(k, v.UserName)
+	}
+	fmt.Println("")
+}
+
